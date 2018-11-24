728f536d6e35edf03397e617736d52bbc4ece3a7
Wesley Graviett <wagkyf@mail.missouri.edu>
2018-11-24 17:14:56 -0600

    modified IP fetch in projectcontents

--- CheckDockerInstalled.sh
+++ CheckDockerInstalled.sh
@@ -4,6 +4,7 @@ if ! [ -x "$(command -v docker)" ]; then #check if docker installed
 	sudo yum install -y docker #installing docker
 	sudo service docker start #Starting Docker
 	sudo mkdir /var/website #Create location for website files
+	sudo chmod 777 /var/website #add permissions to allow auto copy SCP
 	sudo docker run -dit --name my-apache-app -p 8080:80 -v /var/website/:/usr/local/apache2/htdocs/ httpd:2.4 #Setting up apache container
 else
 	echo " Docker already installed. Updating file" #indicates docker installed
