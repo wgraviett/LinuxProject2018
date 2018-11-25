557d17e8b2d0c2d95dc194c02c5443ab8178d050
Wesley Graviett <wagkyf@mail.missouri.edu>
2018-11-24 19:03:51 -0600

    Pushing all finalized content

--- projectRemote.sh
+++ projectRemote.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-CurrentIP=$(ifconfig | grep "inet" | grep -v "inet6" | grep -v "inet 127.0.0.1" | tr -s"" " " | cut -d " " -f3 | grep "192." | cut -d "." -f1,2,3)
-CurrentIPLastOctet=$(ifconfig | grep "inet" | grep -v "inet6" | grep -v "inet 127.0.0.1" | tr -s"" " " | cut -d " " -f3 | grep "192." | cut -d "." -f4)
+CurrentIP=$(ifconfig | grep "inet" | grep -v "inet6" | grep -v "inet 127.0.0.1" | tr -s"" " " | cut -d " " -f3 | grep "192." | cut -d "." -f1,2,3) #collect Three Octets of IP Address. Allow for identification of Class C network.
+CurrentIPLastOctet=$(ifconfig | grep "inet" | grep -v "inet6" | grep -v "inet 127.0.0.1" | tr -s"" " " | cut -d " " -f3 | grep "192." | cut -d "." -f4) #Collect Last Octet. This allow the script to prevent from querying its own information in the for loop. 
 
 echo $CurrentIP #finds Current IP
 echo " Locating Nodes on network..."
 IPPingLocatorLimit=0
 for ((i=1; i<255; i++)) #Check all class C IP address to see available machines.
 do
-	if [ $i -ne $CurrentIPLastOctet ] #Skips Master Host Ping/ssh.
+	if [ $i -ne $CurrentIPLastOctet ] #Skips Master Host Ping/ssh. Skips master nodes IP so it doesn't query itself. 
 	then
-	if [ $IPPingLocatorLimit -ne 2 ] ##used to pause after two are detected. 
+	if [ $IPPingLocatorLimit -ne 2 ] ##used to pause after two nodes are detected. Increase value if you want more nodes.  
 	then
 	$(ping -c 1 $CurrentIP.$i > /dev/null) #verifies active nodes, CurrentIP variable holds 3 octets of ip address, this allows for the for loop to cycle 1 to 255 for any class C address such as 192.168.1.x, or 192.168.2.x
-	if [ $? -eq 0 ] #If node is active, then ssh and receive data. 
+	if [ $? -eq 0 ] #If node is active according to ping results, then ssh and receive data. 
 	then
 		echo " Found: $CurrentIP.$i"
 		echo " Connecting over ssh"
-		ssh -t root@$CurrentIP.$i " $(< projectContents.sh)" >> data.txt
+		ssh -t root@$CurrentIP.$i " $(< projectContents.sh)" >> data.txt#Runs projectcontents script from local device on the remote device
 		let "IPPingLocatorLimit++" #used to count active nodes
 		if [ $IPPingLocatorLimit -ne 2 ] #if 2 nodes have not been found yet, continue
 		then
